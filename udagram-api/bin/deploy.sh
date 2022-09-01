f7d07484a9e47c3c90b68c4810b651e062d01b3c
Lê Văn Tiền <leo1172001@gmail.com>
2022-09-01 22:34:48 +0700

    update project

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,8 +1,5 @@
-eb init Udagramapi --region us-east-1 &&
+eb init Udagramapi --region us-east-1 && eb use Udagramapi-env && eb deploy Udagramapi-env &&
 
-eb use Udagramapi-env &&
-
-eb setenv NAME=$NAME
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 eb setenv POSTGRES_HOST=$POSTGRES_HOST
@@ -15,4 +12,3 @@ eb setenv URL=$URL
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
-&& eb deploy
