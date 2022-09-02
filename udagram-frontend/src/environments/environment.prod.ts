7b1759e8f9cf124ece3d5306b85965b05951d021
Lê Văn Tiền <leo1172001@gmail.com>
2022-09-02 14:53:46 +0700

    update project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://a4bff1c0c74a1467386819245ffcb624-1889933595.eu-central-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://udagrambackend-env.eba-nucyxdnd.us-east-1.elasticbeanstalk.com:8080/api/v0'
 };
 
 /*
