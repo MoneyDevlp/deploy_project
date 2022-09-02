d4558426294660ecbd85b1d6736616d16dae210d
Lê Văn Tiền <leo1172001@gmail.com>
2022-09-02 21:36:56 +0700

    update project

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,19 +1,19 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(
-  "postgres://postgres:postgres@postgres.chngmizyry7x.us-east-1.rds.amazonaws.com:5432/postgres"
-);
+// export const sequelize = new Sequelize(
+//   "postgres://postgres:postgres@database-1.chi4rj1mum6t.us-east-1.rds.amazonaws.com:5432/postgres"
+// );
 
 
-// export const sequelize = new Sequelize({
-//   username: config.username,
-//   password: config.password,
-//   database: config.database,
-//   host: config.host,
-//   port: config.port,
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port,
 
-//   dialect: "postgres",
-//   storage: ":memory:",
-// });
+  dialect: "postgres",
+  storage: ":memory:",
+});
 
